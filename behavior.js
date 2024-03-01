a7d49fb2f9b31f1e70bad97a11ce50bf378326c7
Ema Decot <emadecot03@gmail.com>
2024-03-01 16:01:23 +0100

    fin

--- behavior.js
+++ behavior.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event){
     alert('Hello!');
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.addEventListener("click", function() {
+        thumbnailElement.className = "";
+      });
+      if (thumbnailElement.className == "") {
+        // write here the code that will execute if the image is big
+    }
 });
+  
